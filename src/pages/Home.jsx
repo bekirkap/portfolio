0dc4756f5c2663292f6626d0cba74125a363bbc7
Bekir Kaplan <bekirkaplannnn@gmail.com>
2024-05-12 21:13:41 +0300

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import Stats from "../components/Stats"
+import Testimonials from "../components/Testimonials"
 
 
 const Home = () => {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus corrupti temporibus mollitia natus at, quod perspiciatis cum iste voluptatum quis eum quisquam itaque explicabo dolore, placeat modi repellendus. Nesciunt, aperiam.</div>
-  )
+<div>
+  <Stats/>
+  <Testimonials/>
+</div>  )
 }
 
 export default Home
