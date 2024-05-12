46f8c4426f9923e6008ad90c6c1930ebb22c7e20
Bekir Kaplan <bekirkaplannnn@gmail.com>
2024-05-12 16:44:41 +0300

    loading and products grid

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,20 +1,25 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import ProductCard from "../components/ProductCard"
+import Loading from "../components/Loading"
 
 
 const Products = () => {
   const [products, setProducts] = useState([])
-  const [Loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false)
   const [search, setSearch] = useState("")
   const getData = async()=>{
     try{
+      setLoading(true)
       const {data} = await axios.get(`https://dummyjson.com/products/search?q=${search}`)
       console.log(data);
       setProducts(data.products)
+      
     } 
     catch (error){
         console.log(error);
+      }finally{
+        setLoading(false)
       }
   }
   useEffect(()=>{
@@ -25,9 +30,16 @@ const Products = () => {
     
     <div className=" m-10">
       <h1>all products</h1>
-      {products.map(product => <ProductCard key={product.id} product={product}/>)
-
-    }
+   
+       {loading ? (
+       <Loading/>
+       )
+      :(
+        <div className=" mt-6 grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 xl:gap-x-8">
+          {products.map(product => <ProductCard key={product.id} product={product}/>)}
+          </div>)
+      }
+    
 
 
     </div>
